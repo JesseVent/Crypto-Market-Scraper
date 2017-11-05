b4b3509ee9fe4435069046598387de71fd2c0a28
JesseVent <jessevent@icloud.com>
2017-11-06 01:30:29 +1030

    Complete Refactor
    - Split into different functions
    - Fixed progress bar issues
    - Now determining cryptocurrency name from API call
    - Added crypto tokens rank as of today
    - General cleanup and removal of redundant code

--- crypto-markets.R
+++ crypto-markets.R
@@ -1,123 +1,87 @@
 require(jsonlite)
 require(dplyr)
 require(doSNOW)
-require(doParallel)
 require(lubridate)
-
-# Define parameters
-file <- "~/Desktop/Crypto-Markets.csv"
-cpucore <-
-  as.numeric(detectCores(all.tests = FALSE, logical = TRUE))
-today <- gsub("-", "", today())
-exchange_rate <- fromJSON("https://api.fixer.io/latest?base=USD")
-AUD <- exchange_rate$rates$AUD
-ptm <- proc.time()
-json <-
-  "https://files.coinmarketcap.com/generated/search/quick_search.json"
-coins <- jsonlite::read_json(json, simplifyVector = TRUE)
-length <- as.numeric(length(coins$slug))
-#length <- 50
-range <- 1:length
-coins <- head(arrange(coins, rank), n = range)
-symbol <- coins$slug
-
-# Setup population of URLS we will scrape the history for
-url <-
-  paste0(
-    "https://coinmarketcap.com/currencies/",
-    symbol,
-    "/historical-data/?start=20130428&end=",
-    today
-  )
-baseurl <- c(url)
-urllist <- data.frame(url = baseurl, stringsAsFactors = FALSE)
-attributes <- as.character(urllist$url)
-
-# Start parallel processing
-cluster = makeCluster(cpucore, type = "SOCK", outfile = "")
-registerDoSNOW(cluster)
-
-# Display progress bar
-pb <- txtProgressBar(min = 1, max = length, style = 3)
-
-# Start scraping function
+# Functions ---------------------------------------------------------------
+# Retrieve Coin Listings -----
+getCoins <- function() {
+     library(plyr)
+     today <- gsub("-", "", today())
+     json <- "https://files.coinmarketcap.com/generated/search/quick_search.json"
+     coins <- jsonlite::read_json(json, simplifyVector = TRUE)
+     coins <- data_frame(symbol = coins$symbol, name = coins$name, slug = coins$slug,
+          rank = coins$rank)
+     length <- as.numeric(length(coins$slug))
+     range <- 1:length
+     url <- paste0("https://coinmarketcap.com/currencies/", coins$slug, "/historical-data/?start=20130428&end=",
+          today)
+     baseurl <- c(url)
+     coins$slug <- as.character(baseurl)
+     coins$rank <- as.numeric(coins$rank)
+     return(coins)
+}
+# Scrape Historical Tables -----
 abstracts <- function(attributes) {
-  library(rvest)
-  page <- read_html(attributes)
-  names <-
-    page %>% html_nodes(css = ".col-sm-4 .text-large") %>% html_text(trim = TRUE) %>%
-    replace(!nzchar(.), NA)
-  nodes <-
-    page %>% html_nodes(css = "table") %>% .[1] %>% html_table(fill = TRUE) %>%
-    replace(!nzchar(.), NA)
-  abstracts <- Reduce(rbind, nodes)
-  
-  # Splitting up the scraped names and cleaning them up nicely.
-  abstracts$names <- gsub("\\(||\\n|\\)|\\s\\s", "", names)
-  abstracts$coin <-
-    as.character(strsplit(abstracts$names, " ")[[1]][1])
-  namelength <- as.numeric(lengths(strsplit(abstracts$names, " ")))
-  namearray <- strsplit(abstracts$names, " ")[[1]][2:namelength]
-  abstracts$coinname <-
-    paste(setdiff(namearray, abstracts$coin), collapse = " ")
-  names(abstracts) <-
-    c("date",
-      "open",
-      "high",
-      "low",
-      "close",
-      "volume",
-      "market",
-      "symbol",
-      "coin")
-  return(abstracts)
+     page <- read_html(attributes)
+     names <- page %>% html_nodes(css = ".col-sm-4 .text-large") %>% html_text(trim = TRUE) %>%
+          replace(!nzchar(.), NA)
+     nodes <- page %>% html_nodes(css = "table") %>% .[1] %>% html_table(fill = TRUE) %>%
+          replace(!nzchar(.), NA)
+     abstracts <- Reduce(rbind, nodes)
+     abstracts$symbol <- gsub("\\(||\\n|\\)|\\s\\s", "", names)
+     abstracts$symbol <- as.character(strsplit(abstracts$symbol, " ")[[1]][1])
+     return(abstracts)
+}
+# Cleanup results table -----
+cleanUp <- function(results) {
+     names(results) <- c("symbol", "name", "ranknow", "date", "open", "high", "low",
+          "close", "volume", "market")
+     marketdata <- results
+     marketdata$volume <- gsub("\\,", "", marketdata$volume)
+     marketdata$market <- gsub("\\,", "", marketdata$market)
+     marketdata$volume <- gsub("\\-", "0", marketdata$volume)
+     marketdata$market <- gsub("\\-", "0", marketdata$market)
+     marketdata$close <- gsub("\\-", "0", marketdata$close)
+     marketdata$date <- format(strptime(marketdata$date, format = "%b %d,%Y"), "%Y-%m-%d")
+     marketdata$open <- as.numeric(marketdata$open)
+     marketdata$close <- as.numeric(marketdata$close)
+     marketdata$high <- as.numeric(marketdata$high)
+     marketdata$low <- as.numeric(marketdata$low)
+     marketdata$volume <- as.numeric(marketdata$volume)
+     marketdata$market <- as.numeric(marketdata$market)
+     # Percent variance between open and close rates
+     marketdata$variance <- ((marketdata$close - marketdata$open)/marketdata$close)
+     # spread variance between days high, low and closing
+     marketdata$volatility <- ((marketdata$high - marketdata$low)/marketdata$close)
+     return(marketdata)
 }
 
-# Bind parallel dataframes and transform into results.
-results = foreach(i = range, .combine = rbind) %dopar%
-{
-  setTxtProgressBar(pb, i)
-  abstracts(attributes[i])
-}
+# START CRYPTOCURRENCY SCRAPING SCRIPT ------------------------------------
+# Crypto Scraping Setup ---------------------------------------------------
+file <- "~/Desktop/Crypto-Markets.csv"
+coins <- getCoins()
+length <- as.numeric(length(coins$slug))
+range <- 1:length
+cpucore <- as.numeric(detectCores(all.tests = FALSE, logical = TRUE))
+ptm <- proc.time()
+
+# Parallel process scraping with progress bar -----------------------------
+cluster = makeCluster(cpucore, type = "SOCK")
+registerDoSNOW(cluster)
+pb <- txtProgressBar(max = length, style = 3)
+progress <- function(n) setTxtProgressBar(pb, n)
+opts <- list(progress = progress)
+attributes <- coins$slug
+
+# Combine results and stop clusters ---------------------------------------
+results = foreach(i = range, .options.snow = opts, .combine = rbind, .packages = "rvest") %dopar%
+     abstracts(attributes[i])
 close(pb)
 stopCluster(cluster)
-print(proc.time() - ptm)
 
-# Clean up all the fields.
-names(results) <-
-  c("date",
-    "open",
-    "high",
-    "low",
-    "close",
-    "volume",
-    "market",
-    "name",
-    "symbol",
-    "coin")
-marketdata <- subset(results, select = -c(name))
-marketdata$volume <- gsub("\\,", "", marketdata$volume)
-marketdata$market <- gsub("\\,", "", marketdata$market)
-marketdata$volume <- gsub("\\-", "0", marketdata$volume)
-marketdata$market <- gsub("\\-", "0", marketdata$market)
-marketdata$close <- gsub("\\-", "0", marketdata$close)
-marketdata$date <-
-  format(strptime(marketdata$date, format = "%b %d,%Y"), "%Y-%m-%d")
-marketdata$open <- as.numeric(marketdata$open)
-marketdata$close <- as.numeric(marketdata$close)
-marketdata$high <- as.numeric(marketdata$high)
-marketdata$low <- as.numeric(marketdata$low)
-marketdata$volume <- as.numeric(marketdata$volume)
-marketdata$market <- as.numeric(marketdata$market)
-
-# Percent variance between open and close rates
-marketdata$variance <-
-  ((marketdata$close - marketdata$open) / marketdata$close)
-
-# Market spread variance between days high, low and closing
-marketdata$volatility <-
-  ((marketdata$high - marketdata$low) / marketdata$close)
-
-# Export dataset to CSV and finish timing
-write.csv(marketdata, file)
+# Cleanup results and fix names -------------------------------------------
+coinnames <- data_frame(symbol = coins$symbol, name = coins$name, rank = coins$rank)
+results <- merge(results, coinnames)
+marketdata <- cleanUp(results)
+write.csv(marketdata, file, row.names = FALSE)
 print(proc.time() - ptm)
